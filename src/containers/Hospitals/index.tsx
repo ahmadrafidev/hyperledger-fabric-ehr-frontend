99faf52af61c7463c8e3745bbf973b416b885405
luthfibalaka <luthfibalaka@gmail.com>
2023-05-26 17:23:58 +0700

    fix(button error): fix submission button error

--- src/containers/Hospitals/index.tsx
+++ src/containers/Hospitals/index.tsx
@@ -546,7 +546,7 @@ const HospitalContainer = () => {
                                         Cancel
                                     </button>
                                     <button
-                                        type="submit"
+                                        onClick={handleSubmit}
                                         className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-gray-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-gray-500 duration-300"
                                     >
                                         Edit EHR Data
